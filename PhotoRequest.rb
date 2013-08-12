201549b3ac6dcd8e599d0a6c1677ce808e4901aa
Simon <simon.taranto@gmail.com>
2013-08-12 11:26:32 -0600

    update runit to loop through and get photos, need to update for redirects

--- PhotoRequest.rb
+++ PhotoRequest.rb
@@ -1,4 +1,3 @@
 class PhotoRequest
-  #plantations_ref = "CnRqAAAAq9Ow5AVTAiE_EDI3MSilzLKf0w6-a6BcUnlEwWfteVzkcPYPa3keDX90zZnTmO4BtsM2aY2KQnqxZ4bb1O3YtQWuK7Z-z09e2VZPe1kWw5lbuOQ_Q9CtLB3pXlnl2HU1DsOkKnSa-r0A6lpPvZhLLhIQbNUYRxBFf2MJmy4juYvAjxoUiHMrquHGLsivNBNuNnI51IIbPto"
-  # link = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=" + plantations_ref + "&sensor=false&key=" + key
+  # link = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=" + photoRef + "&sensor=false&key=" + key
 end
--- runit.rb
+++ runit.rb
@@ -1,8 +1,10 @@
 require_relative 'PlaceRequest'
 require 'json'
 require 'pp'
+require 'net/http'
 
 placesOutput = "places.json"
+key = "AIzaSyC2aZ5uDR1lR0ucYpo4PTWV_4pXbZPmow4"
 
 if false
 	req = PlaceRequest.new(reqType = "place",
@@ -25,19 +27,29 @@ placesHash = pp parsed
 
 results = placesHash["results"] # array of hashes
 
+# this works but needs to HANDLE REDIRECTS
+  # try http://stackoverflow.com/questions/6934185/ruby-net-http-following-redirects
 buildings = Hash.new
 results.each do |res|
-  buildings[res["name"]] = res["photos"] if res["photos"] 
+  buildings[res["name"]] = res["photos"] if res["photos"]
+  currentBuilding = res["name"]
+  currentPhotos = buildings[currentBuilding]
+  if !currentPhotos.nil?
+    currentPhotos.each do |photo|
+      photoRef = photo["photo_reference"]
+      link = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=" + photoRef + "&sensor=false&key=" + key
+      puts "LINK IS" + link
+        uri = URI(link)
+        Net::HTTP.start(uri.host, uri.port,
+              :use_ssl => uri.scheme == 'https') do |http|
+          request = Net::HTTP::Get.new uri
+          response = http.request request
+          @resBody = response.body
+          puts @resBody
+        end
+    end
+  end
 end
 
-### send each photoRef to PhotoRequest
-# make a new dir (or find existing one) for school name
-
-## LOOP
-  # through the buildings hash and pull out the NAME and photo reference
-
-  # create http request  
-
   # take response and rename it (# create image name)
-
-  # download the file 
+  # download the file
