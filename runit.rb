7ad91638b3c8b64412d1fab0151f3877b58e922b
Simon <simon.taranto@gmail.com>
2013-08-12 09:14:30 -0600

    remove unnecessary vars

--- runit.rb
+++ runit.rb
@@ -1,10 +1,5 @@
 require_relative 'PlaceRequest'
 
-key = "AIzaSyC2aZ5uDR1lR0ucYpo4PTWV_4pXbZPmow4"
-location = '42.448734650355775,-76.47395993447884'
-radius = "1000"
-sensor = "false"
-
 req = PlaceRequest.new(reqType = "place",
 						key = "AIzaSyC2aZ5uDR1lR0ucYpo4PTWV_4pXbZPmow4",
 						location = '42.448734650355775,-76.47395993447884',
